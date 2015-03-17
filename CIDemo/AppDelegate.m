e2431c1f81847df26e424ab18fd7a0e139bdb994
Shardul Navare <snavare@Shardul.local>
2015-03-17 11:28:18 -0400

    DNS resolved?

--- CIDemo/AppDelegate.m
+++ CIDemo/AppDelegate.m
@@ -24,39 +24,7 @@
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     
-    //This is a test 1
-    // Another commit
-    // Test 2
-    // Test 3
-    // push 1
-    // Test for webhook
-    // Webhook 2
-    // Added service
-    // Testing auto build
-    // TEsting output folder
-    // Testing output folder 2
-    // Checking the Desktop folder creation
-    // checking for MAMP file creation
-    //     // checking for MAMP file creation2
-    // Is it hitting HTDocs?
-    // Folder Test
-    // Version update test
-    //comment
-    //123
-    //commit
-    // Change on 0734
-    // change on 0740
-    // test for job completed on 0744
-    // try again for notification
-    // test for https
-    // test auto update
-    // test for notification 2
-    // 200 response or not
-    // check for notification with log level 10
-    // test for app update
-    // check app update
-    // test for change in name
-    // is name going to remain the same
+    // Test for DNS resolution
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
